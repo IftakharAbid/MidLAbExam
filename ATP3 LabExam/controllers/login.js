a2fdba0c1a594e3993b33b4583d2bed15c6ee50a
Iftakharul Alam Chowdhury <34824315+IftakharAbid@users.noreply.github.com>
2020-02-27 10:59:07 +0600

    Add files via upload

---
+++ ATP3 LabExam/controllers/login.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var userModel = require.main.require('./models/user-model');
+var router = express.Router();
+
+router.get('/', function(req, res){
+	res.render('login/index');
+});
+
+router.post('/', function(req, res){
+
+	var user ={
+		username: req.body.uname,
+		password: req.body.password
+	};
+
+	userModel.validate(user, function(status){
+	 	if(status){
+			res.cookie('username', req.body.uname);
+			res.redirect('/home');
+		}else{
+			res.send('invalid username/password');
+		}
+	});
+});
+
+router.get('/registration', function(req, res){
+	
+		res.render('login/registration');
+	
+});
+
+router.post('/registration', function(req, res){
+	
+		var user = {
+			id: req.body.id,
+			username: req.body.username,
+			password: req.body.password,
+			type: req.body.type
+		};
+
+		userModel.insert(user, function(status){
+			if(status){
+				res.redirect('/login');
+			}else{
+				res.redirect('/login');
+			}
+		});
+});
+
+
+
+
+module.exports = router;
